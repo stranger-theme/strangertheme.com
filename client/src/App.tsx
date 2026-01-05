3c216c03c5030b2b9ab9e3ac38f8c9096e55e51a
Thiago Vieira <thiagotcvieira@gmail.com>
2026-01-05 17:20:00 -0300

    feat: add Analytics component and update dependencies for performance monitoring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Switch, Route } from "wouter"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import { queryClient } from "./lib/queryClient"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { Toaster } from "@/components/ui/toaster"
@@ -27,6 +28,7 @@ function App() {
           <Toaster />
           <Router />
           <SpeedInsights />
+          <Analytics />
         </TooltipProvider>
       </I18nProvider>
     </QueryClientProvider>
