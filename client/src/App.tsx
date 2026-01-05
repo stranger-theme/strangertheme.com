65724fa1d66e9b195af8e1292325c0efe147d418
Thiago Vieira <thiagotcvieira@gmail.com>
2026-01-05 17:17:22 -0300

    feat: add SpeedInsights component to App for performance monitoring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route } from "wouter"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { queryClient } from "./lib/queryClient"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { Toaster } from "@/components/ui/toaster"
@@ -25,6 +26,7 @@ function App() {
         <TooltipProvider>
           <Toaster />
           <Router />
+          <SpeedInsights />
         </TooltipProvider>
       </I18nProvider>
     </QueryClientProvider>
